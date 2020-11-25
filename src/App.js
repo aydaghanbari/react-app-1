4d715199793aca151cc2f05629e056fafd616abc
Ayda Ghanbari <ayda.ghanbari88@gmail.com>
2020-11-25 13:23:58 +0330

    add app-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './app.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    state= {
+        buttonColor: false
+    };
+    handleButtonclick = () => {
+        const buttonColor = this.state.buttonColor
+     this.setState ({buttonColor: !buttonColor})   
+    };
+   
+
+    render () {
+        
+        let textColor = ( <h1 className="red">Hello there</h1>);
+           if (this.state.buttonColor === false) {
+               textColor = (<h1 className="blue">Hello there</h1>);
+            }
+            return (
+                <div>
+                    {textColor}
+                    <button onClick={this.handleButtonclick}>click!</button>
+                </div>
+        );
+        
+    }
 }
-
 export default App;
